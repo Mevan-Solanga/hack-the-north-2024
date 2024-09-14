2417981b4267ba0455ce49172110da63791d12d2
Aung Khant Min <aungkhantmin2014@gmail.com>
2024-09-14 15:36:03 -0400

    add microphone

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from SpeechToText import GroqSpeechToText
 from TextToBraille import to_braille
+from Microphone import SoundDeviceMicrophone
 
 speechToText = GroqSpeechToText()
+microphone = SoundDeviceMicrophone()
 
-text = speechToText.get_text() 
-print(text)
-
-print(to_braille(text))
+while (True):
+    microphone.record()
+    text = speechToText.get_text()
+    print(text)
+    print(to_braille(text))
