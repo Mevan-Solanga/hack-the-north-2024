679fb1a9b85ba268cf4551d92d13c8d169aa5a15
Rachelle Liu <r37liu@uwaterloo.ca>
2024-09-14 18:29:16 -0400

    edit servo test code

--- servo.py
+++ servo.py
@@ -1,15 +1,15 @@
 from gpiozero import Servo
 from time import sleep
 from gpiozero import AngularServo
-#from gpiozero.pins.pigpio import PiGPIOFactory
-#from gpiozero import Device
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Device
 
-#Device.pin_factory = PiGPIOFactory()
+Device.pin_factory = PiGPIOFactory()
 
 servo = AngularServo(17)
 
 while True:
-    servo.angle = -20
+    servo.angle = 75
     sleep(2)
-    servo.angle = 40
+    servo.angle = 50
     sleep(2)
