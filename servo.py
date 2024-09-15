88b7a6a05b669f5454f22e68ccc5c7e3a6cd5885
Rachelle Liu <93688951+rachelleliu@users.noreply.github.com>
2024-09-14 22:59:31 -0400

    servo test

--- servo.py
+++ servo.py
@@ -1,10 +1,16 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(17, min_angle=-90, max_angle=90)
+servo = AngularServo(17, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
 
-while True:
-    servo.angle = 75
-    sleep(2)
-    servo.angle = 45
-    sleep(2)
+def set_angel(angel):
+	servo.angle = angle
+	sleep(1)
+	
+try:
+	while True:
+		angel = int(input("Enter angel (0 to 180): "))
+		set_angle(angle)
+except KeyboardInterrupt:
+	print("Program stopped by user")
+
