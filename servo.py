de3a0e309dddda809a961a05b427228b8d77a6d0
Aung Khant Min <aungkhantmin2014@gmail.com>
2024-09-14 23:51:15 -0400

    servo fix

--- servo.py
+++ servo.py
@@ -3,8 +3,8 @@ class GPIOServo:
     def __init__(self, pin):
         GPIO.setmode(GPIO.BCM)  # Use BCM numbering
         GPIO.setup(pin, GPIO.OUT)
-        pwm = GPIO.PWM(pin, 50)
-        pwm.start(7.5) 
+        self.pwm = GPIO.PWM(pin, 50)
+        self.pwm.start(7.5) 
     
     # Function to move the servo to a specific angle
     def move_to_angle(degree):
@@ -16,9 +16,9 @@ class GPIOServo:
         
         # Convert the angle to duty cycle
         duty = (degree / 18.0) + 2.5  # Map 0-180 degrees to duty cycle 2.5-12.5
-        pwm.ChangeDutyCycle(duty)
+        self.pwm.ChangeDutyCycle(duty)
         time.sleep(0.5)  # Give enough time for the servo to reach the position
-        pwm.ChangeDutyCycle(0)
+        self.pwm.ChangeDutyCycle(0)
 
     def change_state(self, is_up = False):
         if is_up:
@@ -27,6 +27,6 @@ class GPIOServo:
             move_to_angle(0)
 
     def __del__(self):
-        pwm.ChangeDutyCycle(0)
-        pwm.stop()
+        self.pwm.ChangeDutyCycle(0)
+        self.pwm.stop()
         GPIO.cleanup()
